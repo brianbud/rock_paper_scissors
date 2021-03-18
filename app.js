f45da314e973f5a7c8e5b4d2686989ab9863931c
brianbud <brianbuddhadasa@gmail.com>
2021-03-17 21:15:20 -0400

    Add win and lose function in app.js

--- app.js
+++ app.js
@@ -10,5 +10,43 @@ else {
     computerPlay = "scissors";
 }
 
-console.log(computerPlay)
 
+// console.log(computerPlay);
+
+
+
+const playerSelection = "scissors";
+const computerSelection = computerPlay;
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === "Rock" && computerSelection === "scissors") {
+        return ("You used Rock , you win");
+    }
+
+    if (playerSelection === "Paper" && computerSelection === "rock") {
+        return ("You used paper and computer used rock, you win!")
+    }
+
+    if (playerSelection === "scissors" && computerSelection === "paper") {
+        return ("You used scissors and computer used paper, you win!")
+    }
+
+    if (computerSelection === "rock" && playerSelection === "scissors") {
+        return ("You Lose! Rock beats Scissors");
+    }
+
+    if (computerSelection === "paper" && playerSelection === "rock") {
+        return ("You Lose! Paper beats Rock!")
+    }
+
+    if (computerSelection === "scissors" && playerSelection === "paper") {
+        return ("You Lose! Scissors beats Paper")
+    }
+
+    else {
+        return ("You lose!");
+    }
+}
+
+
+console.log(playRound(playerSelection, computerSelection));
